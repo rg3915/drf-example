e03d8bd399cfb5cef100b7f1d2cb6c7a63175920
Regis Santos <regis42santos@gmail.com>
2021-09-03 16:30:08 -0300

    fix: permite autenticação no Swagger

--- backend/settings.py
+++ backend/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
