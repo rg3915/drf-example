dd096f5b1e30e2144710f4d7f74da206897e3dd3
Regis Santos <regis42santos@gmail.com>
2021-09-05 00:24:30 -0300

    feat: Adiciona click ao consumer. close #3

--- consumer.py
+++ consumer.py
@@ -1,18 +1,17 @@
-from typing import Dict
 from pprint import pprint
+from typing import Dict
 
+import click
 import requests
 from requests.auth import HTTPBasicAuth
 
 BASE_URL = 'http://localhost:8000/api/v1/'
 
 
-def fetch_token(session, endpoint):
+def fetch_token(session, endpoint, username, password):
     '''
     Faz autenticação do usuário.
     '''
-    username = 'admin'  # TODO
-    password = 'd'  # TODO
     # headers = {'Content-type': 'application/json'}  # Não precisou
     data = {
         'username': username,
@@ -27,13 +26,13 @@ def fetch_token(session, endpoint):
         return response.json()
 
 
-def get_token() -> Dict[str, str]:
+def get_token(username, password) -> Dict[str, str]:
     '''
     Pega o access_token do usuário logado.
     '''
     with requests.Session() as session:
         endpoint = f'{BASE_URL}auth/jwt/create/'
-        response = fetch_token(session, endpoint)
+        response = fetch_token(session, endpoint, username, password)
         data = {
             'access_token': response['access'],
         }
@@ -49,19 +48,22 @@ def fetch(session, endpoint, access_token):
         return response.json()
 
 
-def main(token):
+@click.command()
+@click.option('--username', '-u', prompt='username', help='Type the username.')
+@click.option('--password', '-p', prompt='password', help='Type the password.')
+def main(username, password):
     '''
     Consumindo a lista de produtos.
     '''
+    token = get_token(username, password)
     access_token = token['access_token']
     with requests.Session() as session:
         endpoint = f'http://127.0.0.1:8000/product/products/'
         response = fetch(session, endpoint, access_token)
+        pprint(response)
         return response
 
 
 if __name__ == '__main__':
-    token = get_token()
-    result = main(token)
     print('Produtos')
-    pprint(result)
+    pprint(main())
