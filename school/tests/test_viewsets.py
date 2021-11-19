305c019fd7e40047bbb32969633c5f6e5712ae17
Regis Santos <regis42santos@gmail.com>
2021-11-19 07:36:04 -0300

    ViewSets with TDD

--- school/tests/test_viewsets.py
+++ school/tests/test_viewsets.py
@@ -50,17 +50,57 @@ class ViewSetTest(TestCase):
         response = self.client.post(
             '/school/students/',
             data=payload,
-            content_type='application/json'
+            # content_type='application/json'
+            format='json'
         )
         esperado = {
-            "id": 1,
+            "id": 2,
             "registration": "1",
             "first_name": "Regis",
             "last_name": "Santos"
         }
         resultado = json.loads(response.content)
         self.assertEqual(esperado, resultado)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_student_retrieve(self):
+        response = self.client.get(
+            '/school/students/1/',
+            content_type='application/json',
+        )
+        resultado = json.loads(response.content)
+        esperado = {
+            "id": 1,
+            "registration": "1",
+            "first_name": "Regis",
+            "last_name": "Santos"
+        }
+        self.assertEqual(esperado, resultado)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_student_partial_update(self):
+        payload = {"registration": "2"}
+        response = self.client.patch(
+            '/school/students/1/',
+            data=payload,
+            format='json'
+        )
+        resultado = json.loads(response.content)
+        esperado = {
+            "id": 1,
+            "registration": "2",
+            "first_name": "Regis",
+            "last_name": "Santos"
+        }
+        self.assertEqual(esperado, resultado)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_student_delete(self):
+        response = self.client.delete(
+            '/school/students/1/',
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_all_students(self):
         response = self.client.get(
